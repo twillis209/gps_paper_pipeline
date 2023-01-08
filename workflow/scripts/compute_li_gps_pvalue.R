c6c28cae6e4c189007630228b3e211b362596768
Tom Willis <tw395@cam.ac.uk>
2023-01-08 12:24:21 +0000

    Fixed issue with Li GPS test script

--- workflow/scripts/compute_li_gps_pvalue.R
+++ workflow/scripts/compute_li_gps_pvalue.R
@@ -1,3 +1,10 @@
-daf <- read.table(snakemake@input[[1]], sep = '\t')
+daf <- read.table(snakemake@input[[1]], sep = '\t', header = T)
 
-write.table(data.frame(gps = daf$GPS, pval = pexp(daf$GPS^-2)), file = snakemake@output[[1]], sep = '\t')
+print(daf)
+
+out_daf <- data.frame(gps = daf$GPS, pval = pexp(daf$GPS^-2))
+
+print(out_daf)
+
+write.table(out_daf, file = snakemake@output[[1]], sep = '\t')
+
